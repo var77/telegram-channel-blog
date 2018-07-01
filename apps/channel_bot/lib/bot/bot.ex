1af9f1d6fef3b2a64834a3529635f15df8694ecc
Varik <varikmatevosyan@gmail.com>
2018-07-01 14:04:46 +0400

    download photo and video send as files

--- apps/channel_bot/lib/bot/bot.ex
+++ apps/channel_bot/lib/bot/bot.ex
@@ -13,7 +13,6 @@ defmodule ChannelBot.Bot do
 
   def handle_update(update) do
     update = update.channel_post
-
     title = Helpers.get_title(update)
     text = Helpers.get_text(update)
     file = Helpers.get_file_id_and_type(update)
@@ -21,6 +20,7 @@ defmodule ChannelBot.Bot do
 
     post = if file do
       path = Helpers.download_file(file.file_id)
+      file = if !file.type, do: Map.put(file, :type, Helpers.get_file_type_from_ext(path)), else: file
       Map.put(post, file.type, path)
     else post
     end
@@ -30,8 +30,8 @@ defmodule ChannelBot.Bot do
   end
 
   def handle_info({:trigger, offset}, state) do
-#    IO.puts :"Checking for updates #{offset}"
-    case Nadia.get_updates(offset: offset, allowed_updates: ["update_id", "channel_post", "edited_channel_post"]) do
+#    IO.puts "Checking for updates #{offset}"
+    case Nadia.get_updates(offset: offset, allowed_updates: ["update_id", "channel_post"]) do
       {:ok, updates} ->
         Enum.each(updates, fn(update) -> spawn(fn -> handle_update(update) end)  end)
         :erlang.send_after(2000, self(), {:trigger, Helpers.get_next_offset(updates)})
